f99752cd571f1f7cd436476e290b6ad578fc9151
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-02-09 21:54:04 -0300

    Solved day 2 problem 2

--- src/day2.rs
+++ src/day2.rs
@@ -27,5 +27,33 @@ pub fn p1() {
 }
 
 pub fn p2() {
-    unimplemented!();
+    let data = BufReader::new(
+        File::open("data/day2.txt").unwrap())
+        .lines();
+
+    let mut position = 0;
+    let mut aim = 0;
+    let mut depth = 0;
+
+    for line in data {
+        let word = line.as_ref().unwrap().split_whitespace().collect::<Vec<&str>>();
+        let direction = word[0];
+        let value: i32 = word[1].parse().unwrap();
+
+        match direction {
+            "forward" => {
+                depth += aim * value;
+                position += value;
+            },
+            "backwards" => {
+                depth -= aim * value;
+                position -= value;
+            },
+            "up" => aim -= value,
+            "down" => aim += value,
+            _ => unreachable!(),
+        };
+    }
+
+    println!("{}", position * depth);
 }
