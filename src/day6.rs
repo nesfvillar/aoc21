c8f1d48b5753689b2ada8813c17c53c00bc579ee
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-02-12 01:36:24 -0300

    Added solution to day 6, part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,8 @@
 const BORN_AGE: usize = 9;
 const CYCLE_TIME: usize = 7;
-const END_TIME: usize = 80;
 
 pub fn p1() {
+    const END_TIME: usize = 80;
     let mut fishes: [usize; BORN_AGE] = include_str!("../data/day6.txt")
         .split(',')
         .map(|n| n.trim().parse::<usize>().unwrap())
@@ -20,5 +20,20 @@ pub fn p1() {
 }
 
 pub fn p2() {
-    unimplemented!();
+    const END_TIME: usize = 256;
+    let mut fishes: [usize; BORN_AGE] = include_str!("../data/day6.txt")
+        .split(',')
+        .map(|n| n.trim().parse::<usize>().unwrap())
+        .fold([0; BORN_AGE], |mut a, n| {
+            a[n] += 1;
+            a
+        });
+
+    for day in 1..END_TIME {
+        fishes[(day + CYCLE_TIME) % BORN_AGE] += fishes[day % BORN_AGE];
+    }
+
+    let result = fishes.into_iter().fold(0, |sum, next| sum + next);
+    println!("Day6, part2 solution: {}", result);
+
 }
